0dfe030f122fbdf9a58079713da27310fd72e06a
Leo Maignan <sookaz@leos-macbook-pro.home>
2020-11-11 21:13:10 +0100

    :pencil2: fix url for online push

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,13 @@
 import "./App.css";
 function Login() {
-    const loginUrl = "https://accounts.spotify.com/authorize?client_id=788a83da87b349d79ab6182bf8593f25&response_type=code&redirect_uri=http://localhost:3000&scope=user-read-recently-played,user-top-read,user-read-playback-position,user-read-playback-state,user-modify-playback-state,user-read-currently-playing,playlist-modify-public,playlist-modify-private,playlist-read-private,playlist-read-collaborative,user-library-modify,user-library-read"
+    const loginUrl = "https://accounts.spotify.com/authorize?client_id=788a83da87b349d79ab6182bf8593f25&response_type=code&redirect_uri=https://new-is-always-better.netlify.app/&scope=user-read-recently-played,user-top-read,user-read-playback-position,user-read-playback-state,user-modify-playback-state,user-read-currently-playing,playlist-modify-public,playlist-modify-private,playlist-read-private,playlist-read-collaborative,user-library-modify,user-library-read"
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get('code');
     if (myParam) {
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({grant_type : "authorization_code", code : myParam, redirect_uri : "http://localhost:3000"})
+            body: JSON.stringify({grant_type : "authorization_code", code : myParam, redirect_uri : "https://new-is-always-better.netlify.app/"})
         };
         fetch('https://accounts.spotify.com/api/token', requestOptions)
             .then(response => console.log(response))
