f17101c1f337e82aab26485aa058d339c4ae7d15
Leo Maignan <sookaz@leos-macbook-pro.home>
2020-11-12 03:20:40 +0100

    :bug: fix bug url online deploy

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Tracklist from './Tracklist.js';
 
 function Login() {
-    const loginUrl = "https://accounts.spotify.com/authorize?client_id=788a83da87b349d79ab6182bf8593f25&response_type=token&redirect_uri=http://localhost:3000&scope=user-read-recently-played,user-top-read,user-read-playback-position,user-read-playback-state,user-modify-playback-state,user-read-currently-playing,playlist-modify-public,playlist-modify-private,playlist-read-private,playlist-read-collaborative,user-library-modify,user-library-read,user-read-recently-played"
+    const loginUrl = "https://accounts.spotify.com/authorize?client_id=788a83da87b349d79ab6182bf8593f25&response_type=token&redirect_uri=https://new-is-always-better.netlify.app&scope=user-read-recently-played,user-top-read,user-read-playback-position,user-read-playback-state,user-modify-playback-state,user-read-currently-playing,playlist-modify-public,playlist-modify-private,playlist-read-private,playlist-read-collaborative,user-library-modify,user-library-read,user-read-recently-played"
     const urlParams = new URLSearchParams(String(window.location.hash).replace("#", "?"));
     const myParam = urlParams.get('access_token');
     if (myParam) {
